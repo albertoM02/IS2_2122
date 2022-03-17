b6433bda0c3b24238ba66f849201fabc3feaa18a
Alberto Moro <amc295@alumnos.unican.es>
2022-03-17 13:26:08 +0100

    Completadas las clases de ImpuestosCirculacionBusiness, último cambio de sesión 2

--- Practica3/ImpuestoCirculacionBusiness/src/main/java/es/unican/is2/impuestoCirculacion/business/business/GestionImpuestoCirculacion.java
+++ Practica3/ImpuestoCirculacionBusiness/src/main/java/es/unican/is2/impuestoCirculacion/business/business/GestionImpuestoCirculacion.java
@@ -1,3 +1,7 @@
+package es.unican.is2.impuestoCirculacion.business.business;
+import es.unican.is2.impuestoCirculacion.common.business.*;
+import es.unican.is2.impuestoCirculacion.common.dao.*;
+import es.unican.is2.impuestoCirculacion.common.dominio.*;
 /**
  * Clase que implementa la capa de negocio de la aplicacion
  */
@@ -12,36 +16,75 @@ public class GestionImpuestoCirculacion implements IGestionContribuyentes, IGest
 	}
 	
 	public Contribuyente altaContribuyente(Contribuyente c) {
-		// TODO
-		return null;
+		contribuyentes.creaContribuyente(c);
+		return c;
 	}
 
 	
 	public Contribuyente bajaContribuyente(String dni) throws OperacionNoValida {
-		// TODO
-		return null;		
+		
+		Contribuyente cExiste = contribuyentes.eliminaContribuyente(dni);
+		
+		if (cExiste == null) {
+			//Caso contribuyente no existe
+			return null;
+		}
+		if (cExiste.getVehiculos().isEmpty() == false) {
+			throw new OperacionNoValida("El contribuyente tiene coches a su nombre.");
+		} else {
+			return cExiste;
+		}
+		
 	 }
 	
 	public Contribuyente contribuyente(String dni) {
-		// TODO
-		return null;
+		return contribuyentes.contribuyente(dni);
 	}
+	
+	
 
 	public Vehiculo altaVehiculo(Vehiculo v, String dni) throws OperacionNoValida {
-		// TODO
-		return null;
+		Contribuyente c = contribuyentes.contribuyente(dni);
+		if (c == null) {
+			//Caso no existe el contribuyente.
+			return null;
+		}
+		if (vehiculos.vehiculo(v.getMatricula()) == null) {
+			//caso no existe vehículo
+			return null;
+		}
+		if (c.getVehiculos().contains(v)) {
+			throw new OperacionNoValida("El contribuyente ya posee este coche.");
+		} else {
+			c.getVehiculos().add(v);
+		}
+		return v;
 	}
 
-	@Override
+	
+	
+	
 	public Vehiculo bajaVehiculo(String matricula, String dni) throws OperacionNoValida {
-		// TODO
-		return null;
+		Contribuyente c = contribuyentes.contribuyente(dni);
+		Vehiculo vBaja = vehiculos.vehiculo(matricula);
+		if (c == null) {
+			//Caso no existe el contribuyente.
+			return null;
+		}
+		if (vBaja == null) {
+			//caso no existe vehículo
+			return null;
+		}
+		if (c.getVehiculos().contains(vBaja)) {
+			c.getVehiculos().remove(vBaja);
+			return vBaja;
+		} else {
+			throw new OperacionNoValida("El contribuyente no posee ese vehiculo.");
+		} 
 	}
 
-	@Override
 	public Vehiculo vehiculo(String matricula) {
-		// TODO
-		return null;
+		return vehiculos.vehiculo(matricula);
 	}	
 }
 
