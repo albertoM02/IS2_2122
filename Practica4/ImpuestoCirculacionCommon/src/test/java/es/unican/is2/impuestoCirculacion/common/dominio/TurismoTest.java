d5d0c32734ca43927172f480c82ba687331c714a
Alberto Moro <amc295@alumnos.unican.es>
2022-03-24 13:27:37 +0100

    Primera sesion terminada

---
+++ Practica4/ImpuestoCirculacionCommon/src/test/java/es/unican/is2/impuestoCirculacion/common/dominio/TurismoTest.java
@@ -0,0 +1,14 @@
+package es.unican.is2.impuestoCirculacion.common.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TurismoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
