60c793c05cf6dfdb8528451606e672c39bf7eb9c
Alberto Moro <amc295@alumnos.unican.es>
2022-04-30 17:52:57 +0200

    Nuevo proyecto Maven para realizar las refactorizaciones.

---
+++ Practica5/GestionTiendaRefactorizado/src/main/java/es/unican/is2/gestionTienda/vendedorEnPracticas.java
@@ -0,0 +1,31 @@
+package es.unican.is2.gestionTienda;
+
+
+public class vendedorEnPracticas extends Vendedor {
+	
+	private String dni;
+	
+	/**
+	 * Retorna un nuevo vendedor en prácticas
+	 * @param nombre
+	 * @param dni
+	 */
+	public vendedorEnPracticas(String nombre, String id, String dni) { //WMC + 1
+		super(nombre, id);
+		this.dni= dni;
+	}
+	
+	public String getDni() { //WMC + 1
+		return dni;
+	}
+
+	@Override
+	public boolean equals(Object obj) { //WMC + 1
+		if (!(obj instanceof vendedorEnPracticas))  //WMC + 1 //CCog + 1
+			return false;
+		vendedorEnPracticas v = (vendedorEnPracticas) obj;
+		return (v.getId().equals(getId()) && v.getDni().equals(getDni())); //WMC + 1 //CCog + 1
+	}
+}
+
+//WMC = 5 // WMCn = 5/3 = 1,66... //CCog = 2
